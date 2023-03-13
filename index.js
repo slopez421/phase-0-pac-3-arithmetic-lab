41e5bdbbc8f6c809fb42de61881b0a5735bd49f7
Shania Lopez <slopez@fandm.edu>
2023-03-12 21:21:11 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b ) {
+
+    return a + b;
+    
+}
+
+function subtract(a, b) {
+    return a-b;
+
+}
+
+function divide(a, b) {
+
+    return a/b;
+
+}
+
+function multiply(a, b) {
+
+    return a*b;
+
+}
+
+function increment(number) {
+   
+    return number + 1;
+    
+}
+
+function decrement(number) {
+
+    return number - 1;
+}
+
+function makeInt(number) {
+
+    return parseInt(number, 10)
+}
+
+function preserveDecimal(number) {
+    
+    return parseFloat(number);
+}
